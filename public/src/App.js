95ba5d318b86682fe383364d6ceefdabacf12354
Ariel Ortuño Solis <arielg.os@gmail.com>
2023-11-17 09:40:22 -0400

    Cleanning Project

--- public/src/App.js
+++ public/src/App.js
@@ -1,29 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
 import { analytics } from './Firebase';
 import { logEvent } from 'firebase/analytics';
+import { useEffect, useState } from 'react';
 
 
 function App() {
 
   logEvent(analytics, 'Application Start');
 
+  const [size, setSize] = useState({ width: window.innerWidth, height: window.innerHeight });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setSize({ width: window.innerWidth, height: window.innerHeight });
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      onClick={() => {
+        console.log('click');
+      }}>
+      <h1>Window Size</h1>
+      <p>Width: {size.width}px</p>
+      <p>Height: {size.height}px</p>
     </div>
   );
 }
